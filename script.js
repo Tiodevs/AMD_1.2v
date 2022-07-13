3f62b0a1c91fd975751e03f65ebec8bb07f21a92
Felipe Pereira dos Santos <87885399+Tiodevs@users.noreply.github.com>
2022-07-13 13:20:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('.content-portfólio',{ duration: 2000});
+
+sr.reveal('.section-1-text',{ duration: 2000});
+
+sr.reveal('.quemsoueu-foto',{ duration: 2000});
+
+sr.reveal('.title-capa',{ duration: 2000});
+
+sr.reveal('.p-capa-2',{ duration: 2000});
+
+sr.reveal('.botaocapa',{ duration: 2000});
+
+
+
